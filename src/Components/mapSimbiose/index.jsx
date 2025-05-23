685549cfb7ac7a1c9dcafe513504749129aa22fc
seu-username <lucasaugusto.pprado@gmail.com>
2025-05-23 20:31:47 -0300

    map att

--- src/Components/mapSimbiose/index.jsx
+++ src/Components/mapSimbiose/index.jsx
@@ -6,7 +6,7 @@ export function Maps() {
     return (
 
         <Container>
-          <iframe src="https://www.google.com/maps/d/embed?mid=1SUeTuq9lHtLKLd08por1KymZnFlp9Ag&ehbc=2E312F" width="100%" height="450"></iframe>
+          <iframe src="https://www.google.com/maps/d/embed?mid=1rUPJSVtxQ3clPK6f_9l6ENtLaXt_DIo&ehbc=2E312F" width="100%" height="450"></iframe>
 
         </Container>
     )
